7c10ceaa74e8c4f9455cc8a816b362696de59772
ashu <ashutoshatoliya@gmail.com>
2023-07-12 18:27:17 +0530

    chat app using react js

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8aM_z-Cumz-r3Xm5lP88ydrojK8gT43c",
+  authDomain: "chatapp-3b128.firebaseapp.com",
+  projectId: "chatapp-3b128",
+  storageBucket: "chatapp-3b128.appspot.com",
+  messagingSenderId: "375243946867",
+  appId: "1:375243946867:web:3215b77d6803d0c55da306",
+  measurementId: "G-LY774TNJSJ"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
